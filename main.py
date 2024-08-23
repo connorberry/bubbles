bc87064cf072d769022e066adaa8476a90f65b44
connorberry <connorberry@yahoo.com>
2024-08-23 11:24:07 -0800

    add clock ticks

--- main.py
+++ main.py
@@ -7,9 +7,13 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
@@ -18,6 +22,8 @@ def main():
                 return
         screen.fill(0)
         pygame.display.flip()
+        ms = clock.tick(60)
+        dt = ms / 1000
 
 if __name__ == "__main__":
 	main()
